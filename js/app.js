b69b84768907f9d1a0a12eea4344bc42a780a8bc
alex kang <akang2@gmail.com>
2016-07-26 11:28:37 -0600

    changed volume to half, added classes for flex, added styling to buttons, holy shit the keyCodes all changed.. q used to be 113 and now it's 81 wtf

--- js/app.js
+++ js/app.js
@@ -4,23 +4,24 @@ $(document).ready(function() {
   });
   $('body').on('keypress', function(e) {
     console.log(e.keyCode);
-    if(e.keyCode == 113) {
+    if(e.keyCode == 81) {
+
       playSound();
     }
-    if(e.keyCode == 119) {
+    if(e.keyCode == 87) {
       playSound2();
     }
   });
 });
 
 function playSound() {
-  $('.sound')[0].volume = 1;
+  $('.sound')[0].volume = 0.5;
   $('.sound')[0].load();
   $('.sound')[0].play();
 }
 
 function playSound2() {
-  $('.sound')[1].volume = 1;
+  $('.sound')[1].volume = 0.5;
   $('.sound')[1].load();
   $('.sound')[1].play();
 }
