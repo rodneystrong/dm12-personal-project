22eb2907c84d41d965ce7335aae8f503c48607f3
aleeex <akang2@gmail.com>
2016-07-25 18:13:34 -0600

    added code to for sound to play on 'e' keypress + refactored some vendor prefixes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
-  $('h1').on('click', function() {
+  $('.lcd').on('click', function() {
     playSound();
+  })
+  $('body').on('keypress', function(e) {
+    console.log(e.keyCode);
+    if(e.keyCode == 101) {
+      console.log(e.keyCode);
+      playSound();
+    }
   })
 });
 
 function playSound() {
-  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].volume = 1;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
