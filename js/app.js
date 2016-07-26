6f32285d7e63f7679328388a587c79fc2a0acfda
alex kang <akang2@gmail.com>
2016-07-26 12:06:47 -0600

    changed keypress to keydown to account for changing keycodes, added keyup class animation, added 2 more audio files to html, coded new audio files to play on keydown in js

--- js/app.js
+++ js/app.js
@@ -2,14 +2,26 @@ $(document).ready(function() {
   $('.lcd').on('click', function() {
     playSound();
   });
-  $('body').on('keypress', function(e) {
+  $('body').on('keydown', function(e) {
     console.log(e.keyCode);
     if(e.keyCode == 81) {
-
+      $('.q').toggleClass('touch');
       playSound();
     }
     if(e.keyCode == 87) {
       playSound2();
+    }
+    if(e.keyCode == 69) {
+      playSound3();
+    }
+    if(e.keyCode == 82) {
+      playSound4();
+    }
+  });
+  $('body').on('keyup', function(e) {
+    if(e.keyCode == 81) {
+      $('.q').toggleClass('touch');
+      //playSound();
     }
   });
 });
@@ -24,4 +36,16 @@ function playSound2() {
   $('.sound')[1].volume = 0.5;
   $('.sound')[1].load();
   $('.sound')[1].play();
+}
+
+function playSound3() {
+  $('.sound')[2].volume = 0.5;
+  $('.sound')[2].load();
+  $('.sound')[2].play();
+}
+
+function playSound4() {
+  $('.sound')[3].volume = 0.5;
+  $('.sound')[3].load();
+  $('.sound')[3].play();
 }
