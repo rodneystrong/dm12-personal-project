23ae1babd54e6be80ba59b5cf9c6fd7313c1e73b
alex kang <akang2@gmail.com>
2016-07-26 21:13:05 -0600

    added letter representation to buttons in html, added styles for new letter

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
     playSound();
   });
   $('body').on('keydown', function(e) {
-    console.log(e.keyCode);
+    console.log(e.which);
     // if(e.keyCode == 81) {
     //   $('.q').toggleClass('touch');
     //   playSound();
     // }
-    switch(e.keyCode) {
+    switch(e.which) {
       //top left
       case 81:
       $('.q').toggleClass('touch');
@@ -25,6 +25,11 @@ $(document).ready(function() {
       case 82:
       $('.r').toggleClass('touch');
       playSound(3);
+      break;
+
+      case 20 && 81:
+      $('.q').toggleClass('touch');
+      playSound(9);
       break;
 
       //bot left
@@ -79,12 +84,12 @@ $(document).ready(function() {
       case 76:
       $('.l').toggleClass('touch');
       playSound(15);
-      break;
     }
   });
-  
+
   $('body').on('keyup', function(e) {
     switch(e.keyCode) {
+      //top left
       case 81:
       $('.q').toggleClass('touch');
       break;
@@ -96,6 +101,48 @@ $(document).ready(function() {
       break;
       case 82:
       $('.r').toggleClass('touch');
+      break;
+
+      //bot left
+      case 65:
+      $('.a').toggleClass('touch');
+      break;
+      case 83:
+      $('.s').toggleClass('touch');
+      break;
+      case 68:
+      $('.d').toggleClass('touch');
+      break;
+      case 70:
+      $('.f').toggleClass('touch');
+      break;
+
+      //top right
+      case 89:
+      $('.y').toggleClass('touch');
+      break;
+      case 85:
+      $('.u').toggleClass('touch');
+      break;
+      case 73:
+      $('.i').toggleClass('touch');
+      break;
+      case 79:
+      $('.o').toggleClass('touch');
+      break;
+
+      //bot right
+      case 72:
+      $('.h').toggleClass('touch');
+      break;
+      case 74:
+      $('.j').toggleClass('touch');
+      break;
+      case 75:
+      $('.k').toggleClass('touch');
+      break;
+      case 76:
+      $('.l').toggleClass('touch');
     }
   });
 });
