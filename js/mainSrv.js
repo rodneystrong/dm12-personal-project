d490b63d7ea8eb35841e6a17ba7e4232fbfaa89d
aleeex <akang2@gmail.com>
2016-07-27 19:23:13 -0600

    added all battle assets and added styles/code for the page

--- js/mainSrv.js
+++ js/mainSrv.js
@@ -172,9 +172,59 @@ angular
           $('.stage-l').toggleClass('hide');
         }
       });
+
+//Battle page
+      $('.ryu').on('mouseenter', function() {
+  		$('.ryu-still').hide();
+  		$('.ryu-ready').show();
+    	})
+    	.on('mouseleave', function() {
+    		$('.ryu-ready').hide();
+    		$('.ryu-still').show();
+    	})
+    	//when the user clicks on ryu
+    	.on('mousedown', function() {
+    		playHadouken();
+    		$('.ryu-ready').hide();
+    		$('.ryu-throwing').show();
+    		$('.hadouken').finish().show()
+    			.animate({
+    				'left': '300px'
+    			},
+    			500,
+    			function() {
+    				$(this).hide();
+    				$(this).css('left', '-212px');
+    			}
+    			);
+    	})
+    	.on('mouseup', function() {
+    		$('.ryu-throwing').hide();
+    		$('.ryu-ready').show();
+    		$('.hadouken').hide();
+    	});
+    	//the keyboard 'x' function for ryu-cool
+    	$("body").on('keydown', function(e) {
+    		if(e.which==88) {
+    			$('.ryu-still').hide();
+    			$('.ryu-ready').hide();
+    			$('.ryu-cool').show();
+    		}
+    	})
+    	//when the user lets go of the 'x' key
+    	.on('keyup', function() {
+    		$('.ryu-cool').hide();
+    		$('.ryu-still').show();
+    	});
+
       function playSound(item) {
         $('.sound')[item].volume = 0.2;
         $('.sound')[item].load();
         $('.sound')[item].play();
+      }
+      function playHadouken () {
+        $('#hadouken-sound')[16].volume = 0.5;
+        $('#hadouken-sound')[16].load();
+        $('#hadouken-sound')[16].play();
       }
   });
