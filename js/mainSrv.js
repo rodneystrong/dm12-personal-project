40b2f0774011babdfb4dc12798e7b5a5b5665aa5
aleeex <akang2@gmail.com>
2016-07-28 11:27:24 -0600

    figured out that you can't compare an array to an array. you gotta set the array into a string and compare to a string.

--- js/mainSrv.js
+++ js/mainSrv.js
@@ -7,6 +7,18 @@ angular
     };
 
     var touched = [];
+    var allTouched = 'abcdefghijklmnop';
+    //sort this 1st array when doing comparison
+    // ,'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'
+
+    //if touched === allTouched {$('battle').toggleClass('hide')}
+    function checkTouched() {
+        var sortedTouched = touched.sort().join('');
+        if(sortedTouched === allTouched) {
+          $('.battle').toggleClass('hide');
+        }
+    }
+
     //create an empty array
     //push a string representing each keystroke to the array
     //when a specific key is pressed
@@ -14,8 +26,8 @@ angular
     //either all the keypress items or a specific # of items.
     //when the array matches the specific condition then addClass
     //or toggleClass to make link visible.
-//Battle page
-
+    //create a 2nd array with all the correct values, then
+    //compare it to your original
 
     $('body').on('keydown', function(e) {
         //console.log(e.which);
@@ -116,72 +128,104 @@ angular
           case 81:
           $('.q').toggleClass('touch');
           $('.stage-q').toggleClass('hide');
+          touched.push('a');
+          checkTouched();
           break;
           case 87:
           $('.w').toggleClass('touch');
           $('.stage-w').toggleClass('hide');
+          touched.push('b');
+          checkTouched();
           break;
           case 69:
           $('.e').toggleClass('touch');
           $('.stage-e').toggleClass('hide');
+          touched.push('c');
+          checkTouched();
           break;
           case 82:
           $('.r').toggleClass('touch');
           $('.stage-r').toggleClass('hide');
+          touched.push('d');
+          checkTouched();
           break;
 
           //bot left
           case 65:
           $('.a').toggleClass('touch');
           $('.stage-a').toggleClass('hide');
+          touched.push('e');
+          checkTouched();
           break;
           case 83:
           $('.s').toggleClass('touch');
           $('.stage-s').toggleClass('hide');
+          touched.push('f');
+          checkTouched();
           break;
           case 68:
           $('.d').toggleClass('touch');
           $('.stage-d').toggleClass('hide');
+          touched.push('g');
+          checkTouched();
           break;
           case 70:
           $('.f').toggleClass('touch');
           $('.stage-f').toggleClass('hide');
+          touched.push('h');
+          checkTouched();
           break;
 
           //top right
           case 89:
           $('.y').toggleClass('touch');
           $('.stage-y').toggleClass('hide');
+          touched.push('i');
+          checkTouched();
           break;
           case 85:
           $('.u').toggleClass('touch');
           $('.stage-u').toggleClass('hide');
+          touched.push('j');
+          checkTouched();
           break;
           case 73:
           $('.i').toggleClass('touch');
           $('.stage-i').toggleClass('hide');
+          touched.push('k');
+          checkTouched();
           break;
           case 79:
           $('.o').toggleClass('touch');
           $('.stage-o').toggleClass('hide');
+          touched.push('l');
+          checkTouched();
           break;
 
           //bot right
           case 72:
           $('.h').toggleClass('touch');
           $('.stage-h').toggleClass('hide');
+          touched.push('m');
+          checkTouched();
           break;
           case 74:
           $('.j').toggleClass('touch');
           $('.stage-j').toggleClass('hide');
+          touched.push('n');
+          checkTouched();
           break;
           case 75:
           $('.k').toggleClass('touch');
           $('.stage-k').toggleClass('hide');
+          touched.push('o');
+          checkTouched();
           break;
           case 76:
           $('.l').toggleClass('touch');
           $('.stage-l').toggleClass('hide');
+          touched.push('p');
+          checkTouched();
         }
       });
 
